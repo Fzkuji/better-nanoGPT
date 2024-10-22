7a63667c9fde9d887efbd91cd160b3ca564c3b9f
FzKuji <fzkuji@gmail.com>
2024-10-22 13:12:46 +0800

    Update train_gpt2.py

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -2,15 +2,15 @@
 # launch as the following (e.g. in a screen session) and wait ~5 days:
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
-wandb_log = False
+wandb_log = True
 wandb_project = 'owt'
 wandb_run_name='gpt2-124M'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 batch_size = 12
-block_size = 1024
-input_size = 2048  # size of the input to the model
+block_size = 256
+input_size = 1024  # size of the input to the model
 gradient_accumulation_steps = 5 * 8
 
 # model
@@ -25,7 +25,7 @@ lr_decay_iters = 600000
 # eval stuff
 eval_interval = 1000
 eval_iters = 200
-log_interval = 1
+log_interval = 10
 
 
 # weight decay
